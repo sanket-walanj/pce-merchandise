44bc00d10ddcbc9a970a006a3514bbb056bae52a
sanket-walanj <code.sanky@gmail.com>
2022-04-18 10:55:57 +0530

    Form

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const merchTabs = document.querySelectorAll('.merch-header-img');
-const merchContents = document.querySelectorAll('.merch-content-card');
+window.addEventListener('DOMContentLoaded', function () {
+  const merchTabs = document.querySelectorAll('.merch-header-img');
+  const merchContents = document.querySelectorAll('.merch-content-card');
 
-merchTabs.forEach((tab, index) => {
-  tab.addEventListener('click', () => {
-    merchContents.forEach((content) => {
-      content.classList.remove('active');
+  merchTabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      merchContents.forEach((content) => {
+        content.classList.remove('active');
+      });
+      merchTabs.forEach((tab) => {
+        tab.classList.remove('active');
+      });
+      merchContents[index].classList.add('active');
+      merchTabs[index].classList.add('active');
     });
-    merchTabs.forEach((tab) => {
-      tab.classList.remove('active');
-    });
-    merchContents[index].classList.add('active');
-    merchTabs[index].classList.add('active');
   });
 });
